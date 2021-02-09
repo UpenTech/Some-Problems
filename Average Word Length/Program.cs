ff473e15c92fed11712c5e136bdafc9d7b2b4af4
ndeu <gautamupen99@gmail.com>
2021-02-09 23:04:24 +0545

    New Problem solved

---
+++ Average Word Length/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Average_Word_Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your sentence or a paragraph: ");
+            string input = Console.ReadLine();     //Input passed
+            
+           //Calulating the total letters in the paragraph
+            float total = input.Replace(" ", "").Length;
+            //Calculating the total words in the paragraph
+            float G = input.Split(" ").GetLength(0);
+            //Rounding off the result to the nearest integral value
+            Console.WriteLine(MathF.Round(total/G));
+            Console.ReadKey()
+            
+            
+
+        }
+    }
+}
